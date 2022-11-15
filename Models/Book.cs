cd4f2b5b252f6cb44ebf0d4afa79bf315fafb25e
Denisa Onetiu <denisaonetiu@gmail.com>
2022-11-15 15:22:59 +0200

    Laborator5

--- Models/Book.cs
+++ Models/Book.cs
@@ -23,6 +23,7 @@ namespace Onetiu_Denisa_Lab2.Models
 
         public int? PublisherID { get; set; }
         public Publisher? Publisher { get; set; } //navigation property
+       
         public ICollection<BookCategory>? BookCategories { get; set; }
     }
 
