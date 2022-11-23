2fb86984d6ef9197d4ebe7bc250bb5d86481b096
Denisa Onetiu <denisaonetiu@gmail.com>
2022-11-23 11:42:01 +0200

    Laborator 6

--- Program.cs
+++ Program.cs
@@ -4,8 +4,19 @@ using Onetiu_Denisa_Lab2.Data;
 using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy =>
+   policy.RequireRole("Admin"));
+});
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options => {
+    options.Conventions.AuthorizeFolder("/Books");
+    options.Conventions.AllowAnonymousToPage("/Books/Index");
+    options.Conventions.AllowAnonymousToPage("/Books/Details");
+    options.Conventions.AuthorizeFolder("/Members", "AdminPolicy");
+});
 builder.Services.AddDbContext<Onetiu_Denisa_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Onetiu_Denisa_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Onetiu_Denisa_Lab2Context' not found.")));
 
@@ -17,6 +28,7 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("Onetiu_Denisa_La
 throw new InvalidOperationException("Connection string 'Onetiu_Denisa_Lab2Context' not found." )));
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 options.SignIn.RequireConfirmedAccount = true)
+ .AddRoles<IdentityRole>()
  .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
